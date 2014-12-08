693078c1a8ff2653a1ae59a2a5d3664400b7df29
minocys <harich.leung@gmail.com>
2014-12-08 15:43:38 -0500

    finished recursion stringify and getelements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
+var getElementsByClassName = function(className, node){
   // your code here
+  var result = [];
+  node = node || document.body;
+
+  var split = node.className.split(' ');
+  if( split.indexOf( className ) >= 0 ){
+    result.push(node);
+  }
+
+  //iterate
+  for ( var x = 0; x < node.children.length; x++ ){
+    var childResults = getElementsByClassName(className, node.children[x]);
+    result = result.concat(childResults);
+  }
+
+  return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,27 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(Array.isArray(obj)){
+    var results = [];
+    for(var x = 0; x < obj.length; x++){
+      results.push( stringifyJSON(obj[x]));
+    }
+    return '[' + results.join(',') +']';
+  }
+
+  if(obj && typeof obj === 'object'){
+    var results = [];
+    for (var key in obj){
+      if( obj[key] === undefined || typeof(obj[key]) === 'function' ){
+        continue;
+      }
+      results.push( stringifyJSON(key)+':'+stringifyJSON(obj[key]) );
+    }
+    return '{' + results.join(',') + '}';
+  }
+
+  if( typeof obj === 'string' ){
+    return '"'+obj+'"';
+  }
+  return ''+obj;
 };
